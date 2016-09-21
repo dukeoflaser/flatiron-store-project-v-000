d22e676e2a5c35fd65dd15b7ee6c432eebc2d9f3
Nathaniel Miller <dukeoflaser@gmail.com>
2016-09-21 12:58:02 -0700

    Cart model specs now pass with addition of add_item and total methods.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,18 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
+
+  def total
+    self.items.map(&:price).inject(0, :+)
+  end
+
+  def add_item(item_id)
+    line_item = LineItem.find_by(item_id: item_id)
+
+    if line_item.nil?
+      LineItem.new(item_id: item_id, cart_id: self.id)
+    else
+      line_item
+    end
+  end
 end
